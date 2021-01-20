6ce9318bc64424e69d2af2295588160661b67b51
Francis0312 <pacotron2001@gmail.com>
2021-01-20 13:42:58 -0600

    added main comment

---
+++ src/ProjectMain.java
@@ -0,0 +1,26 @@
+
+/**
+ * Application that plays a hand-written song using JFugue 5.0's library.
+ * This is my submission for my semester-long project for my music class.
+ * in Spring 2021.
+ * 
+ * @author Francisco Reyna 
+ * University of Texas at Austin Computer Science
+ */
+
+ // TODO - implement GUI that has a drop down menu for several songs and a play button
+
+public class ProjectMain {
+    
+    // Constants
+
+    public static void main(String[] args) {
+        System.out.println("Running ... ");
+        
+    }
+
+    public static void playSong(String song) {
+
+    }
+
+}
